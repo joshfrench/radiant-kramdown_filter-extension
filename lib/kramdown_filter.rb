85d97d24dc8e904fd087d91b9d5376ee917d81b1
johnmuhl <git@johnmuhl.com>
2010-01-26 09:30:51 -0600

    wrap SmartyPants with kramdown for prettier output

--- lib/kramdown_filter.rb
+++ lib/kramdown_filter.rb
@@ -1,6 +1,6 @@
 class KramdownFilter < TextFilter
   description_file File.dirname(__FILE__) + "/../markdown.html"
   def filter(text)
-    Kramdown::Document.new(text).to_html
+    Kramdown::Document.new(RubyPants.new(text).to_html).to_html
   end
 end
